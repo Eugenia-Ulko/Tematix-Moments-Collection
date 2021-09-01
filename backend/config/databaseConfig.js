ce295be4d0f77e8e511aea0b0af1cb12e1e1cabe
Matryoshka.es <eugenia.ulko@gmail.com>
2021-09-01 19:16:34 +0200

    Configure Mongo and mongoose

---
+++ backend/config/databaseConfig.js
@@ -0,0 +1,17 @@
+const { connect } = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const connection = await connect(process.env.MONGO_URI,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+
+    console.log(`Mongo connected: ${connection.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
+
+module.exports = connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const excursions = require('./excursions');
 
