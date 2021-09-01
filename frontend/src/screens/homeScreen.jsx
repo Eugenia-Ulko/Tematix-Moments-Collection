907199bb90f136519a1041ffb32332a1a296f51b
Matryoshka.es <eugenia.ulko@gmail.com>
2021-09-01 17:03:05 +0200

    Set up backend and frontend connection with proxy

--- frontend/src/screens/homeScreen.jsx
+++ frontend/src/screens/homeScreen.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
+
+import axios from 'axios';
 import Excursion from '../components/Excursions/excursion';
-import excursions from '../excursions';
 
-const HomeScreen = () => (
-  <>
+const HomeScreen = () => {
+  const [excursions, setExcursions] = useState([]);
 
-    <h1>Highlight tours</h1>
-    <Row>
-      {excursions.map((excursion) => (
-        <Col key={excursion.id} sm={12} md={6} lg={3} xl={3}>
-          <Excursion excursion={excursion} />
-        </Col>
-      ))}
+  useEffect(() => {
+    const fetchExcursions = async () => {
+      const { data } = await axios.get('/api/excursions');
 
-    </Row>
-  </>
-);
+      setExcursions(data);
+    };
+    fetchExcursions();
+  }, []);
 
+  return (
+    <>
+
+      <h1>Highlight tours</h1>
+      <Row>
+        {excursions.map((excursion) => (
+          <Col key={excursion.id} sm={12} md={6} lg={3} xl={3}>
+            <Excursion excursion={excursion} />
+          </Col>
+        ))}
+
+      </Row>
+    </>
+  );
+};
 export default HomeScreen;
