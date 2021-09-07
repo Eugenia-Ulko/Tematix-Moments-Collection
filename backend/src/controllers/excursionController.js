fab637e5eb140af98a564a03b125cb1311087aa4
Matryoshka.es <eugenia.ulko@gmail.com>
2021-09-07 14:51:42 +0200

    Configure excursion controllers

---
+++ backend/src/controllers/excursionController.js
@@ -0,0 +1,28 @@
+const asyncHandler = require('express-async-handler');
+const Excursion = require('../models/excursionModel');
+
+// @desc   Fetch all excursions
+// @route GET /api/excursions
+// @access  Public
+const getExcursions = asyncHandler(async (req, res) => {
+  const excursions = await Excursion.find({});
+  res.json(excursions);
+});
+
+// @desc   Fetch single excursion
+// @route GET /api/excursions/:id
+// @access  Public
+const getExcursionById = asyncHandler(async (req, res) => {
+  const excursion = await Excursion.findById(req.params.id);
+  if (excursion) {
+    res.json(excursion);
+  } else {
+    res.status(404);
+    throw new Error('Excursion not found');
+  }
+});
+
+module.exports = {
+  getExcursions,
+  getExcursionById
+};
--- backend/src/routes/excursionRouter.js
+++ backend/src/routes/excursionRouter.js
@@ -1,29 +1,15 @@
 const { Router } = require('express');
-const asyncHandler = require('express-async-handler');
-const Excursion = require('../models/excursionModel');
+const {
+  getExcursions,
+  getExcursionById
+} = require('../controllers/excursionController');
 
 const excursionRouter = new Router();
 
-// @desc   Fetch all excursions
-// @route GET /api/excursions
-// @access  Public
 excursionRouter
-  .get('/', asyncHandler(async (req, res) => {
-    const excursions = await Excursion.find({});
-    res.json(excursions);
-  }));
+  .route('/').get(getExcursions);
 
-// @desc   Fetch single excursion
-// @route GET /api/excursions/:id
-// @access  Public
 excursionRouter
-  .get('/:id', asyncHandler(async (req, res) => {
-    const excursion = await Excursion.findById(req.params.id);
-    if (excursion) {
-      res.json(excursion);
-    } else {
-      res.status(404).json({ message: 'Excursion not found' });
-    }
-  }));
+  .route('/:id').get(getExcursionById);
 
 module.exports = excursionRouter;
