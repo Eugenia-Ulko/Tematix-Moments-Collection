96ffae3924a68ee32fda1d3cb817c89c7da493cf
Matryoshka.es <eugenia.ulko@gmail.com>
2021-09-08 11:18:04 +0200

    Add create user function

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 const asyncHandler = require('express-async-handler');
-const generateToken = require('../utils/generateToken');
+const { generateToken } = require('../utils/generateToken');
 const User = require('../models/userModel');
 
 // @desc   Auth user ¨get a token
@@ -25,4 +25,54 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { authUser };
+// @desc   Register a new user
+// @route POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+  const user = await User.create({
+    name,
+    email,
+    password
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id)
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
+// @desc   Get user profile
+// @route GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+module.exports = { authUser, registerUser, getUserProfile };
--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -1,11 +1,16 @@
 const { Router } = require('express');
 const {
-  authUser
+  authUser, registerUser, getUserProfile
 } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
 
 const userRouter = new Router();
 
 userRouter
+  .route('/').post(registerUser);
+userRouter
   .post('/login', authUser);
+userRouter
+  .route('/profile').get(protect, getUserProfile);
 
 module.exports = userRouter;
